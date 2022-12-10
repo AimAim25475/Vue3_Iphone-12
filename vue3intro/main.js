c7b62cb18da80df9b5316b6ee0fdf4664fb4c232
AimAim25475 <118649114+AimAim25475@users.noreply.github.com>
2022-12-10 23:02:14 +0700

    Add files via upload

---
+++ vue3intro/main.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+
+    // เรียกใช้ฟังก์ชัน data สำหรับเก็บตัวแปร
+    data() {
+      return {
+        product: 'iPhone 12',
+        brand: 'Apple',
+        image: './assets/images/iphone-12-red.png',
+        description: 'New Apple A14 Bionic hight perfomance',
+        url: 'https://www.apple.com',
+        inStock: true,
+        inventory: 20,
+        details: ['64GB', '128GB', '256GB'],
+        variants: [
+          {id: 239, color: 'Red', image: './assets/images/iphone-12-red.png'},
+          {id: 240, color: 'Purple', image: './assets/images/iphone-12-purple.png'},
+          {id: 250, color: 'White', image: './assets/images/iphone-12-white.png'},
+        ],
+        cart: 0
+      }
+    },
+  
+    methods: {
+
+      addToCart() {
+        this.cart += 1
+        this.inventory -= 1
+      },
+      updateImage(variantImage){
+        this.image = variantImage;
+      }
+    },
+  
+    computed: {
+      title() {
+        return this.brand + ' ' + this.product
+      }
+    }
+  
+  }).mount('#app')
